ffa1932b556a4636039a95b61330123e3ad8c5e5
Egon <egonstep@hotmail.com>
2019-08-25 16:36:57 -0300

    dice general 4

--- MyApplication5/app/src/main/java/com/example/dicegeneral/MainActivity.java
+++ MyApplication5/app/src/main/java/com/example/dicegeneral/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dicegeneral;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -12,10 +13,13 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
+@SuppressLint("DefaultLocale")
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textScore, textSequence, textPlay;
+    private TextView textSequence, textPlay;
     private Set<String> sections;
 
     @Override
@@ -23,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textScore = findViewById(R.id.textScore);
         textSequence = findViewById(R.id.textSequence);
         textPlay = findViewById(R.id.textPlay);
 
@@ -35,17 +38,15 @@ public class MainActivity extends AppCompatActivity {
         printSequence(dices, textPlay);
 
         String resultClassification = getDicesThrowsResult(dices);
-        int resultPoints = getPoints(resultClassification, dices);
 
-        textScore.setText(String.format("Score - %d", resultPoints));
         textSequence.setText(String.format("Classification - %s", resultClassification));
     }
 
     private List<Integer> throwDices(int unit) {
         List<Integer> dices = new ArrayList<>();
         for (int i = 0; i < unit; i++) {
-            int var = (int) (Math.random() * 6) + 1;
-            dices.add(var);
+            int dice = (int) (Math.random() * 6) + 1;
+            dices.add(dice);
         }
         return dices;
     }
@@ -53,98 +54,92 @@ public class MainActivity extends AppCompatActivity {
     private void printSequence(List<Integer> dices, TextView textPlay) {
         textPlay.setText("");
         for (int i = 0; i < dices.size(); i++) {
-            textPlay.append(String.format("Dice %d is %d\n", i+1, dices.get(i)));
+            textPlay.append(String.format("Dice %d is %d\n", i + 1, dices.get(i)));
         }
     }
 
     private String getDicesThrowsResult(List<Integer> dices) {
-        Map<Integer, Integer> countMap = new HashMap<>();
+        Map<Integer, Integer> countDices = new HashMap<>();
 
         for (Integer dice : dices) {
-            if (countMap.containsKey(dice))
-                countMap.put(dice, countMap.get(dice) + 1);
+            if (countDices.containsKey(dice))
+                countDices.put(dice, countDices.get(dice) + 1);
             else
-                countMap.put(dice, 1);
+                countDices.put(dice, 1);
         }
 
-        return getClassification(countMap);
+        return getClassification(countDices, dices);
     }
 
-    private int getPoints(String result, List<Integer> dices) {
-        if (result.equalsIgnoreCase("Full House"))
-            return 25;
-        else if (result.equalsIgnoreCase("Yahtzee"))
-            return 50;
-        else if (result.equalsIgnoreCase("Three Of A Kind")
-                || result.equalsIgnoreCase("Four Of A Kind")
-                || result.equalsIgnoreCase("Chance")) {
-            int sum = 0;
-            for (Integer d : dices) {
-                sum += d;
-            }
-            return sum;
-        } else if (result.equalsIgnoreCase("Small Straight"))
-            return 40;
-        else if (result.equalsIgnoreCase("Large Straight"))
-            return 30;
-        else
-            return 0;
-    }
+    private String getClassification(Map<Integer, Integer> countMap, List<Integer> dices) {
+        SortedSet<Integer> sortDices = new TreeSet<>(countMap.keySet());
 
-    private String getClassification(Map<Integer, Integer> countMap) {
         if (isFullHouse(countMap))
-            return "Full House";
+            return "Full House scored 25 points";
         else if (isYahtzee(countMap))
-            return "Yahtzee";
+            return "GREAT! Yahtzee scored 50 points";
         else if (isThreeOfAKind(countMap))
-            return "Three Of A Kind";
+            return String.format("Three Of A Kind scored %d points", getSumFromDices(dices));
         else if (isFourOfAKind(countMap))
-            return "Four Of A Kind";
-        else if (isStraight(countMap)) {
-            if (countMap.containsKey(1))
-                return "Small Straight";
-            else
-                return "Large Straight";
-        } else
-            return isUpperSection(countMap);
+            return String.format("Four Of A Kind scored %d points", getSumFromDices(dices));
+        else if (isSmallStraight(sortDices))
+            return "Small Straight scored 40 points";
+        else if (isLargeStraight(sortDices))
+            return "Large Straight scored 30 points";
+        else
+            return isUpperSection(countMap, dices);
     }
 
-    private boolean isStraight(Map<Integer, Integer> countMap) {
-        return countMap.containsValue(1) && countMap.size() == 5
-                && sections.add("Small Straight") || sections.add("Large Straight");
+    private int getSumFromDices(List<Integer> dices) {
+        int sum = 0;
+        for (Integer d : dices) {
+            sum += d;
+        }
+        return sum;
     }
 
-    private boolean isFourOfAKind(Map<Integer, Integer> countMap) {
-        return countMap.containsValue(4) && sections.add("Four Of A Kind");
-    }
-
-    private boolean isThreeOfAKind(Map<Integer, Integer> countMap) {
-        return countMap.containsValue(3) && sections.add("Three Of A Kind");
+    private boolean isFullHouse(Map<Integer, Integer> countMap) {
+        return countMap.containsValue(2) && countMap.containsValue(3)
+                && sections.add("Full House");
     }
 
     private boolean isYahtzee(Map<Integer, Integer> countMap) {
         return countMap.containsValue(5) && sections.add("Yahtzee");
     }
 
-    private boolean isFullHouse(Map<Integer, Integer> countMap) {
-        return countMap.containsValue(2) && countMap.containsValue(3) && sections.add("Full House");
+    private boolean isThreeOfAKind(Map<Integer, Integer> countMap) {
+        return countMap.containsValue(3) && sections.add("Three Of A Kind");
     }
 
-    private String isUpperSection(Map<Integer, Integer> countMap) {
+    private boolean isFourOfAKind(Map<Integer, Integer> countMap) {
+        return countMap.containsValue(4) && sections.add("Four Of A Kind");
+    }
+
+    private boolean isSmallStraight(SortedSet<Integer> sort) {
+        return sort.first() == 1 && sort.last() == 5
+                && sort.size() == 5 && sections.add("Small Straight");
+    }
+
+    private boolean isLargeStraight(SortedSet<Integer> sort) {
+        return sort.first() == 2 && sort.last() == 6
+                && sort.size() == 5 && sections.add("Large Straight");
+    }
+
+    private String isUpperSection(Map<Integer, Integer> countMap, List<Integer> dices) {
         if (countMap.containsKey(6) && sections.add("Sixes"))
-            return "Sixes";
+            return String.format("Sixes scored %d points", countMap.get(6) * 6);
         else if (countMap.containsKey(5) && sections.add("Fives"))
-            return "Fives";
+            return String.format("Fives scored %d points", countMap.get(5) * 5);
         else if (countMap.containsKey(4) && sections.add("Fours"))
-            return "Fours";
+            return String.format("Fours scored %d points", countMap.get(4) * 4);
         else if (countMap.containsKey(3) && sections.add("Threes"))
-            return "Threes";
+            return String.format("Threes scored %d points", countMap.get(3) * 3);
         else if (countMap.containsKey(2) && sections.add("Twos"))
-            return "Twos";
+            return String.format("Twos scored %d points", countMap.get(2) * 2);
         else if (countMap.containsKey(1) && sections.add("Aces"))
-            return "Aces";
+            return String.format("Aces scored %d points", countMap.get(1));
         else if (sections.add("Chance"))
-            return "Chance";
+            return String.format("Chance scored %d points", getSumFromDices(dices));
         else
             return "Points Already Marked";
     }
